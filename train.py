54a980fe458caa7425f37afc750110964c037812
Corey Zumar <corey.zumar@databricks.com>
2019-03-31 23:04:56 -0700

    Remove commented code

--- train.py
+++ train.py
@@ -73,7 +73,6 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-# model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
